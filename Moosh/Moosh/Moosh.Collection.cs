aad592b7dcfa49c0fe99d5d1b6d840fc975ef2bf
regiostech <ultralammed@yahoo.com>
2015-09-28 22:41:05 -0400

    Go?

---
+++ Moosh/Moosh/Moosh.Collection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moosh
+{
+	/// <summary>
+    /// A collection of objects that can be simultaneously controlled.
+    /// </summary>
+    public class Collection<T> : List<T>
+    {
+    }
+}
